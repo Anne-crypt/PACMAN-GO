2969add66f82c857e3d6d10593c50fe7f4a4ca77
rahim pauillac <pauillacrahim@gmail.com>
2021-08-24 17:02:10 +0200

    landing page first

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#landing'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'pages#home'
   resources :games, only: [:show, :create, :new]
   resources :player, only: [:create, :new]
 end
