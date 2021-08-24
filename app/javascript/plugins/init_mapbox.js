715c8c6de79887aba40480f2e85f9aeaa0a04de0
Dorien Reynders <dorien.reynders@hotmail.com>
2021-08-24 17:02:02 +0200

    finetuning

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,13 +3,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const place_marker = (lat, lon, map) => {
   new mapboxgl.Marker().setLngLat([lon, lat]).addTo(map);
-  console.log(`test ${lat} ${lon} `)
-}
-
-const set_my_position = (location) => {
-  const lon = location.coords.longitude;
-  const lat = location.coords.latitude;
-};
+ }
 
 const initMapbox = () => {
   const mapElement = document.getElementById('gamemap');
@@ -25,7 +19,6 @@ const initMapbox = () => {
       interactive: false
     });
 
-
     const element = document.createElement('div');
     element.className = 'marker';
     element.style.backgroundImage = `url('https://placekitten.com/g/25/25/')`;
@@ -33,9 +26,6 @@ const initMapbox = () => {
     element.style.width = '25px';
     element.style.height = '25px';
     new mapboxgl.Marker(element).setLngLat([2.379993, 48.8640313]).addTo(map);
-
-
-
 
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition((position) => {
