f07755ffde29c7ff70b30e92421e6b245fe447a8
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-24 13:42:41 +0200

    generated players list on new game page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "..."
+puts "Cleaning database..."
+Player.destroy_all
+
+puts "..."
+puts "Creating players..."
+anne = Player.new(nickname: "Anne", color: "blue")
+raph = Player.new(nickname: "Rapkalin", color: "red")
+dorien = Player.new(nickname: "Dodo", color: "pink")
+rahim = Player.new(nickname: "Rahim", color: "Orange")
+
+players = [anne, raph, dorien, rahim]
+
+players.each do |player|
+  player.save!
+  puts "#{player.nickname} has been created"
+end
+
+puts "You have created #{players.count} players"
+puts "..."
+puts "You are good to go! :)"
