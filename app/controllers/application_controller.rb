c6676a0189d4cc68cc618316cba294f1c32a4792
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-26 17:22:32 +0200

    user session fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
     # la méthode va associer le user connecté à la variable current_player
     # Cette méthode s'appelle de la mémoisation
     # # On pourra garder cette méthode
-    # if session[:player_id]
-    #   @current_player ||= Player.find_by(id: session[:player_id])
-    # end
+    if session[:player_id]
+      @current_player ||= Player.find_by(id: session[:player_id])
+    end
 
     # To activate only if you want to log as Host
-    Player.find_by(nickname: "Anne")
+    # Player.find_by(nickname: "Anne")
   end
   # Ce helper permet que le current player dans les views
   # /!\ A garder sous la methode current_player
