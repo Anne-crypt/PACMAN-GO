e78ec7df79b3427b5c7d4cb5e0f924e6fb0b64bf
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-27 15:06:21 +0200

    js select guest host done

---
+++ app/javascript/controllers/hover_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from 'stimulus';
+
+export default class extends Controller {
+  static targets = ['hostSelect', 'guestSelect'];
+
+  // connect() {
+  //   console.log('Hello from your first Stimulus controller');
+  // }
+
+  // select(event) {
+  //   if (event.currentTarget.dataset.role == 'guest') {
+  //     this.generatedTarget.classList.add('d-none');
+  //     this.tokenTarget.classList.remove('d-none');
+  //   } else {
+  //     console.log('Hello world!');
+  //   }
+  // }
+
+  select(event) {
+    if (event.currentTarget.dataset.role == 'guest') {
+      event.currentTarget.classList.add('settings-hover');
+      this.hostSelectTarget.classList.remove('settings-hover');
+    } else {
+      event.currentTarget.classList.add('settings-hover');
+      this.guestSelectTarget.classList.remove('settings-hover');
+    }
+  }
+}
