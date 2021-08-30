ab17fe8e150c1d37c8a50fe81b686c8cc850b1f9
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-30 10:17:37 +0200

    removing game token from player controller and pages/show (home)

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,7 +5,7 @@ class PlayersController < ApplicationController
     @player.latitude = rand(48.865171..48.865433)
     @player.longitude = rand(2.379320..2.379690)
 
-    if @player.save!
+    if @player.save
       session[:player_id] = @player.id
       if params['tokens']['token'].present?
         token = params['tokens']['token']
@@ -21,7 +21,7 @@ class PlayersController < ApplicationController
       @participation.save!
       redirect_to edit_game_path(@game.id)
     else
-      render :new
+      render "pages/home"
     end
   end
 
--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,6 @@ class Player < ApplicationRecord
   has_many :games, dependent: :destroy
   has_many :participations, dependent: :destroy
 
+  validates_length_of :nickname, minimum: 2, maximum: 8, allow_blank: false, acceptance: { message: 'nickname must be between 2 and 8 characters max.' }
+
 end
