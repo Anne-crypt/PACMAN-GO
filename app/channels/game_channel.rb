00441ad44e87b98968af5f35ec79c5870dfa04ed
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-31 10:19:10 +0200

    websocket OK

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -2,6 +2,6 @@ class GameChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     game = Game.find(params[:id])
-    stream_from game
+    stream_for game
   end
 end
--- app/javascript/channels/gameroom_channel.js
+++ app/javascript/channels/gameroom_channel.js
@@ -2,15 +2,15 @@ import consumer from './consumer';
 
 const initGameroomCable = () => {
   const playersContainer = document.getElementById('players-list');
-  console.log(playersContainer);
+  // console.log(playersContainer);
   if (playersContainer) {
     const id = playersContainer.dataset.gameId;
     consumer.subscriptions.create(
       { channel: 'GameroomChannel', id: id },
       {
         received(data) {
-          playerContainer.insertAdjacentHTML('beforeend', data);
-          console.log(data); // called when data is broadcast in the cable
+          playersContainer.outerHTML = data;
+          // console.log(data); // called when data is broadcast in the cable
         },
       }
     );
