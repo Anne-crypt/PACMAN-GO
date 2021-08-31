91d591ebcbdd149e49131f419380c2bb227751c2
Raphael Kalinowski <Raphael@myminicom.com>
2021-08-31 18:59:51 +0200

    controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -11,10 +11,10 @@ class PlayersController < ApplicationController
     pacman = @game.participations.find_by(role: 'pacman').player
     gameover = @game.players.joins(:participations).where(participations: { role: 'ghost' })
                             .near([pacman.latitude, pacman.longitude], 0.005).any?
-    if gameover?
-      @game.finished = true
-      GamestatusChannel.broadcast_to(@game)
-    end
+    # if gameover?
+    #   @game.finished = true
+    #   GamestatusChannel.broadcast_to(@game)
+    # end
 
     GameChannel.broadcast_to(@game, @player)
   end
