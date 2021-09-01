38852fdac0a72a3ca9a0d536c990fdaabbb51510
Dorien Reynders <dorien.reynders@hotmail.com>
2021-09-01 17:58:14 +0200

    delete prybug as require

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-require "pry-byebug"
-
 class ApplicationController < ActionController::Base
   add_flash_types :info
 
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,3 @@
-require "pry-byebug"
-
 class PlayersController < ApplicationController
 
   def update
