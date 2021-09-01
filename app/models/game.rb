058690d9c963b559f858961890e48224ac1724d2
Raphael Kalinowski <Raphael@myminicom.com>
2021-09-01 09:43:51 +0200

    added limit 4 players

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :player
   has_many :items, dependent: :destroy
-  has_many :participations, dependent: :destroy
+  has_many :participations, dependent: :destroy, length: { is: 4 }
   has_many :players, through: :participations
 
   COLORS=["red", "blue", "orange", "pink", "green"]
