7a91dc798fc440e440cfb3f0ac1407740fa2cfb1
Raphael Kalinowski <Raphael@myminicom.com>
2021-09-01 09:54:45 +0200

    limit and alert done

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -52,16 +52,20 @@ class PlayersController < ApplicationController
         @participation = Participation.new(game_id: @game.id,
           player_id: @player.id,
           role: @game.player == current_player ? "pacman" : "ghost")
-        @participation.save!
-        # Broadcast action cable
-        @game.participations.each do |participation|
-          ParticipationChannel.broadcast_to(
-            participation,
-            render_to_string(partial: "players",
-            locals: { receiver_participation: participation } )
-          )
+       if @participation.save
+          # Broadcast action cable
+          @game.participations.each do |participation|
+            ParticipationChannel.broadcast_to(
+              participation,
+              render_to_string(partial: "players",
+              locals: { receiver_participation: participation } )
+            )
+          end
+          redirect_to edit_game_path(@game)
+        else
+          flash[:alert] = "OOPS! Party is already full"
+          redirect_to home_path
         end
-        redirect_to edit_game_path(@game)
       end
     else
       flash[:alert] = "OOPS! Something went wrong, try again! Make sure "
